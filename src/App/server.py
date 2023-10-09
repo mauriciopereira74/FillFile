ffe9f84f8e48a62399b3bb5c9bc235ee3e2aa507
mauriciopereira74 <mauriciovianense@hotmail.com>
2023-10-09 10:14:52 +0100

    socket working

--- src/App/server.py
+++ src/App/server.py
@@ -1,3 +1,10 @@
 import socket
 
- s = socket.socket(socket.AF_INET,socket.SOCK_STREAM)
+s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+s.bind((socket.gethostname(),1234))
+s.listen(5)
+
+while True:
+    clientsocket, address = s.accept()
+    print(f"Connection from {address} has been estabilished!")
+    clientsocket.send(bytes("Welcome to the server!", "utf-8"))
