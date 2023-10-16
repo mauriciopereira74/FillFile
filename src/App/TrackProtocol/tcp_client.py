9666716632a6663c01777bc2bb06af0ebdc2c3e1
mauriciopereira74 <mauriciovianense@hotmail.com>
2023-10-16 10:26:29 +0100

    mkdir

--- src/App/TrackProtocol/tcp_client.py
+++ src/App/TrackProtocol/tcp_client.py
@@ -1,4 +1,15 @@
 import socket
+import os
+
+folder_path= '../../../cache'
+
+try:
+    os.mkdir(folder_path)
+except FileExistsError:
+    pass
+
+result = os.listdir(folder_path)
+
 
 client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 client.connect((socket.gethostname(),1111))
