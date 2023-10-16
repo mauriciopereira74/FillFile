c493fd56ce64f1f6663abfb6f55806339d4de8b4
mauriciopereira74 <mauriciovianense@hotmail.com>
2023-10-16 10:00:45 +0100

    save nodes

--- src/App/TrackProtocol/tcp_client.py
+++ src/App/TrackProtocol/tcp_client.py
@@ -3,7 +3,5 @@ import socket
 client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 client.connect((socket.gethostname(),1111))
 
-client.send("Hello Server!".encode('utf-8'))
-print(client.recv(1024).decode('utf-8'))
-client.send("Bye!".encode('utf-8'))
-print(client.recv(1024).decode('utf-8'))
+name = client.recv(1024).decode('utf-8')
+print(name)
