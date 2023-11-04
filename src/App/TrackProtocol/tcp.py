fcc8c3974c5d7fa2013444b87283a1a6e308b2c8
mauriciopereira74 <mauriciovianense@hotmail.com>
2023-11-04 20:34:50 +0000

    tipo 2 working

--- src/App/TrackProtocol/tcp.py
+++ src/App/TrackProtocol/tcp.py
@@ -10,13 +10,15 @@ def run_server():
     server.listen()
 
     connected_clients = []
+    file_locator = {}
+    file_sizes = {}
+    available_files = []
 
     while True:
         clientsocket, address = server.accept()
         print(f"Connected to {address}")
 
-        client_name = f"FS_NODE{len(connected_clients) + 1}"
-        connected_clients.append((client_name, address))
+        connected_clients.append(address)
 
         full_msg = b''
 
@@ -27,7 +29,7 @@ def run_server():
         message_type = int.from_bytes(message_type_bytes, byteorder='big')
 
         if message_type == 1:
-            #adiconar tamanho dos ficheiros
+            # Adicionar tamanho dos ficheiros
             length_temp = clientsocket.recv(2)
             list_length = int.from_bytes(length_temp, byteorder='big')
 
@@ -41,12 +43,37 @@ def run_server():
                     break
                 full_msg += chunk
 
-            files_list = full_msg.decode("utf-8").split('|')
+            files_data = full_msg.decode("utf-8").split('|')
 
-            print(f"Received files list: {files_list}")
+            # Adicionar arquivos e tamanhos ao dicionário file_sizes
+            for file_data in files_data:
+                file, size = file_data.split(',')
+                file_sizes[file] = int(size)
+
+            # Adicionar arquivos ao dicionário file_locator
+            for file in file_sizes:
+                if file not in file_locator:
+                    file_locator[file] = [address[0]]
+                else:
+                    file_locator[file].append(address[0])
+
+            # Atualizar a lista de arquivos disponíveis
+            available_files = list(file_sizes.keys())
+
+            print(f"File sizes: {file_sizes}")
+            print(f"File locator: {file_locator}")
+            print(f"Available files: {available_files}")
 
         if message_type == 2:
-            pass
+            available_files_str = '|'.join(available_files)
+            available_files_bytes = available_files_str.encode("utf-8")
+            length = len(available_files_bytes)
+
+            length_bytes = length.to_bytes(2, byteorder='big')
+
+            packet = length_bytes + available_files_bytes
+
+            clientsocket.sendall(packet)
 
         clientsocket.close()
 
@@ -54,7 +81,8 @@ def run_server():
 def run_client():
     # ip_address, port, directory = input("Enter [ip address] [port] [directory]: \n").split()
     # port = int(port)
-    ip_address = "192.168.1.233"
+
+    ip_address = "127.0.0.1"
     port = 1111
     directory = "../../../cache"
 
@@ -64,17 +92,17 @@ def run_client():
     files_list = os.listdir(directory)
     files_list.sort()
 
-    message_type = 1
-
+    message_type = 2
 
     if message_type == 1:
         length = len(files_list)
 
         message_type_bytes = message_type.to_bytes(1, byteorder='big')
         length_bytes = length.to_bytes(2, byteorder='big')
-        port_bytes = port.to_bytes(2,byteorder='big')
+        port_bytes = port.to_bytes(2, byteorder='big')
 
-        files_list_str = '|'.join(files_list)
+        files_data = [f"{file},{os.path.getsize(os.path.join(directory, file))}" for file in files_list]
+        files_list_str = '|'.join(files_data)
         files_list_bytes = files_list_str.encode("utf-8")
 
         packet = message_type_bytes + length_bytes + port_bytes + files_list_bytes
@@ -87,6 +115,20 @@ def run_client():
         packet = message_type_bytes
 
         client.send(packet)
+
+        # Receive the list of available files from the server
+        length_bytes = client.recv(2)
+        length = int.from_bytes(length_bytes, byteorder='big')
+        available_files_bytes = client.recv(length)
+        available_files_str = available_files_bytes.decode("utf-8")
+        available_files = available_files_str.split('|')
+        print(f"Available files: {available_files}")
+
+    if message_type == 3:
+        
+        pass
+
+    client.close()
 
 
 if __name__ == "__main__":
